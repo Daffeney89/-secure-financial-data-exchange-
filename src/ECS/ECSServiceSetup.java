aa41726273eab30a035c4605c7e7e3e278cacf53
Daffeney <145998665+Daffeney89@users.noreply.github.com>
2024-06-21 00:34:01 +0100

    Create ECSServiceSetup.java
    ECSService

---
+++ src/ECS/ECSServiceSetup.java
@@ -0,0 +1,9 @@
+package com.example.ecs;
+
+public class ECSServiceSetup {
+    // Assume this class contains methods to set up ECS services
+    public void setupService() {
+        System.out.println("Setting up ECS Service...");
+        // Logic to configure and deploy ECS service
+    }
+}
